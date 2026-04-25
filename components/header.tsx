01d6b5c2df6d10201d7bffaa0e0652647935f73b
정예은 <jeong-yeeun@jeong-yeeun-ui-MacBookPro.local>
2026-04-25 21:15:37 +0900

    fix logo

--- components/header.tsx
+++ components/header.tsx
@@ -59,7 +59,7 @@ export function Header() {
         {/* 로고 */}
         <Link href="/" className="flex items-center">
           <Image
-            src="/hsu-logo.png"
+            src="/hsu-logo.pngv=2"
             alt="한성대학교 로고"
             width={220}
             height={55}
